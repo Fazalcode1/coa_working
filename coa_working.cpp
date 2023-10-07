3d14f49f4b20ad1c4cc8817ca2e9d785721be367
Fazalcode1 <126341873+Fazalcode1@users.noreply.github.com>
2023-10-07 22:27:52 +0530

    Update coa_working.cpp

--- coa_working.cpp
+++ coa_working.cpp
@@ -1,57 +1,59 @@
-#include <bits/stdc++.h>
- 
-using namespace std;
+#include <iostream>
+#include <vector>
 
-class edge{
-public:
-	int32_t source;
-	int32_t destination;
-	double weight;
-	
-	edge(int32_t src,int32_t dst,double wt)
-	: source(src),destination(dst),weight(wt){}
-};
-
-class vertex{
-public:
-	int32_t number;
-	vector<edge> neighbours;
-	
-    vertex(){
-        number = -1;
-    }
+class Graph
+{
+    using WeightTy = double;
+    using VertexTy = std::size_t;
+    VertexTy numVertices;
     
-	vertex(int32_t num): number(num){}
-	
-	void add_edge(edge& Edge){
-		neighbours.push_back(Edge);
-	}
+    std::vector<VertexTy> csrList;
+    std::vector<WeightTy> csrWeights;
+    std::vector<std::size_t> csrOffsets; 
+public:
+    Graph(VertexTy numVertices_, std::vector<VertexTy> &&csrList_, std::vector<WeightTy> &&csrWeights_) : 
+    numVertices(numVertices_), csrList(std::move(csrList_)), csrWeights(std::move(csrWeights_))
+    {
+        csrOffsets.resize(numVertices_+1, 0);
+        for(VertexTy i=0;i<csrList.size();i++){
+            csrOffsets[csrList[i]+1]++;
+        }
+        for(VertexTy i=1;i<numVertices_+1;i++){
+            csrOffsets[i]+=csrOffsets[i-1];
+        }
+    }
 };
 
-class graph{
-public:
-	int32_t total_vertices;
-	int32_t total_edges;
-	unordered_map<int32_t,vertex> vertices;
-	vector<edge> edges;
-	
-	void store(vector<vector<pair<int32_t,int32_t>>>& adj){
-		int32_t total_vertices = adj.size();
-		total_edges = 0;
-		for(int32_t i=0;i<total_vertices;i++){
-			vertex v(i);
-			for(int32_t j=0;j<adj[i].size();j++){
-				edge e(i,adj[i][j].first,adj[i][j].second);
-				v.add_edge(e);
-				edges.push_back(e);
-				total_edges++;
-			}
-            vertices[i] = v;
-		}
-	}
-	
-	vector<edge> getNeighbours(int32_t V){
-        vertex w = vertices[V];
-		return w.neighbours;
-	}
-};
+int main(){
+    using std::cin;
+    using WeightTy = double;
+    using VertexTy = std::size_t;
+    std::string s1,s2,s3,s4;
+    VertexTy numNodes, numEdges;
+    std::vector<VertexTy> csrList;
+    std::vector<WeightTy> csrWeights;
+    cin>>s1>>s2>>s3;
+    cin>>numNodes;
+    std::vector<std::vector<std::pair<VertexTy,WeightTy>>> adj(numNodes+1);
+    cin>>s3;
+    cin>>numEdges;
+    for(VertexTy i=0;i<numEdges;i++){
+        cin>>s4;
+        VertexTy src,dest;
+        WeightTy wt;
+        cin>>src>>dest>>wt;
+        adj[src-1].push_back({dest-1,wt});
+        adj[dest-1].push_back({src-1,wt});
+    }
+    for(VertexTy i=0;i<numNodes;i++){
+        for(auto x:adj[i]){
+            csrList.push_back(x.first);
+            csrWeights.push_back(x.second);
+        }
+    }
+    Graph g(numNodes,std::move(csrList),std::move(csrWeights));
+    // for(int i=0;i<g.csrOffsets.size();i++){
+    //     std::cout<<g.csrOffsets[i]<<" ";
+    // }
+
+}
